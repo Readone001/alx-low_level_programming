d34d098c389c6d10dae145318f31f80776f95ac6
Readone001 <ridwanshotonwa@gmail.com>
2023-10-05 15:44:52 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,3 @@
-%s/\s\+$//e
-
 #include <unistd.h>
 
 /**
@@ -11,7 +9,7 @@
 int main(void)
 
 {
-char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";	
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, str1, 59);
 return (1);
 }
