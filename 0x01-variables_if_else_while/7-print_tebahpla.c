be085d418f006ba603f75ee4e037c06b234ed2ee
Readone001 <ridwanshotonwa@gmail.com>
2023-10-06 14:40:52 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Prints reversed alphabet.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+
+{
+char lowerCase;
+
+for (lowerCase = 'z'; lowerCase >= 'a'; lowerCase--)
+
+{
+putchar(lowerCase);
+}
+
+putchar('\n');
+return (0);
+}
