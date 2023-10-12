c538c68934b0b63a6772181077bf92685013ce54
Readone001 <ridwanshotonwa@gmail.com>
2023-10-12 14:09:22 -0700

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,20 +12,20 @@ void print_square(int size)
 int co, ro;
 
 if (size <= 0)
-	
+
 {
 _putchar('\n');
 }
 
 else
-	
+
 {
 for (co = 1; co <= size; co++)
-		
+
 {
 _putchar('#');
 for (ro = 2; ro <= size; ro++)
-			
+
 {
 _putchar('#');
 }
