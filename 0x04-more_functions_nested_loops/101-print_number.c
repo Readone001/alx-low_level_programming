7df9e05a75c0e916ba83ba9af762712556bd21f0
Readone001 <ridwanshotonwa@gmail.com>
2023-10-12 13:53:21 -0700

    Task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -12,7 +12,7 @@ void print_number(int n)
 {
 unsigned int m, d, count;
 
-if (n < 0)	
+if (n < 0)
 
 {
 _putchar(45);
@@ -36,7 +36,7 @@ count *= 10;
 }
 
 for (; count >= 1; count /= 10)
-	
+
 {
 _putchar(((m / count) % 10) + 48);
 }
