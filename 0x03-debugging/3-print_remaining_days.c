38694b6fdbf39fd64fbc2592c46c5f565f893ae2
Readone001 <ridwanshotonwa@gmail.com>
2023-10-12 13:08:47 -0700

    Task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -6,7 +6,7 @@
 * left in the year, taking leap years into account
 * @month: month in number format
 * @day: day of month
-* @year: year 
+* @year: year
 * Return: void
 */
 
@@ -14,10 +14,10 @@ void print_remaining_days(int month, int day, int year)
 
 {
 if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
-	
-{		
+
+{
 if (month > 2 && day >= 60)
-		
+
 {
 day++;
 }
@@ -27,14 +27,14 @@ printf("Remaining days: %d\n", 366 - day);
 }
 
 else
-	
+
 {
 if (month == 2 && day == 60)
-		
+
 {
 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
-		
+
 else
 
 {
