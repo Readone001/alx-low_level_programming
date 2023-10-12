04b82ad7369204b57eb0e740bc9fb16a05ca3d98
Readone001 <ridwanshotonwa@gmail.com>
2023-10-12 13:48:32 -0700

    Task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+* main -prints prime factors of 1231952
+*
+* Return: Always 0 (success)
+*/
+
+int main(void)
+
+{
+long int n, pf;
+
+n = 612852475143;
+for (pf = 2; pf <= n; pf++)
+	
+{
+if (n % pf == 0)
+		
+{
+n /= pf;
+pf--;
+}
+}
+
+printf("%ld\n", pf);
+return (0);
+}
