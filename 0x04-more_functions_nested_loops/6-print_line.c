544490119f41e8300392181be2e611efdf19ae49
Readone001 <ridwanshotonwa@gmail.com>
2023-10-12 14:11:20 -0700

    Task 6

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,20 +12,20 @@ void print_line(int n)
 int co;
 
 if (n <= 0)
-	
+
 {
 _putchar('\n');
 }
-	
+
 else
-	
+
 {
 for (co = 1; co <= n; co++)
-		
+
 {
 _putchar('_');
 }
-		
+
 _putchar('\n');
 }
 }
