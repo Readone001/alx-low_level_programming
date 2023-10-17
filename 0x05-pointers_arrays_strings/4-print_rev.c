2201c001853920086a216c2c46f9f9d958e46a01
Readone001 <ridwanshotonwa@gmail.com>
2023-10-17 16:48:24 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_rev - Prints a string, in reverse, followed by a new line.
+* @s: input string.
+* Return: no return.
+*/
+
+void print_rev(char *s)
+
+{
+int len = 0;
+
+while (s[len] != '\0')
+len++;
+
+while (len)
+_putchar(s[--len]);
+
+_putchar('\n');
+}
